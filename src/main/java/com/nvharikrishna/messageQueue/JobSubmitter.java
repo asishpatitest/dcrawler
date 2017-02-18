e67f6cec64c2a55f471edaa5ae9b23552c3ba5d7
asishpatitest <pati.asish14@gmail.com>
2017-02-18 18:02:52 +0530

    Add Kafka classes

---
+++ src/main/java/com/nvharikrishna/messageQueue/JobSubmitter.java
@@ -0,0 +1,12 @@
+package com.nvharikrishna.messageQueue;
+
+/**
+ * Created by abinash on 18/2/17.
+ */
+public class JobSubmitter {
+
+    public static void main(String args[]) {
+
+    }
+
+}
---
+++ src/main/java/com/nvharikrishna/messageQueue/UrlFilterStream.java
@@ -0,0 +1,7 @@
+package com.nvharikrishna.messageQueue;
+
+/**
+ * Created by abinash on 18/2/17.
+ */
+public class UrlFilterStream {
+}
