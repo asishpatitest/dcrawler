8be33fca825ed6ca9c432e5f1cbcb51457c3d84b
nvharikrishna <hari_nv2001@yahoo.co.in>
2017-02-18 15:51:00 +0530

    Added singleton class for Jedis client.

---
+++ src/test/java/com/nvharikrishna/cache/JedisClientTest.java
@@ -0,0 +1,14 @@
+package com.nvharikrishna.cache;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Harikrishna on 18/02/17.
+ */
+public class JedisClientTest {
+    @org.junit.Test
+    public void getJedis() throws Exception {
+
+    }
+
+}
