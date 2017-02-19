0e6f7f108450eb0fd830c36f3a79f977f0b25eb1
asishpatitest <pati.asish14@gmail.com>
2017-02-19 16:46:15 +0530

    Add kafka stream

--- src/main/java/com/nvharikrishna/messageQueue/KafkaUtils.java
+++ src/main/java/com/nvharikrishna/messageQueue/KafkaUtils.java
@@ -1,6 +1,8 @@
 package com.nvharikrishna.messageQueue;
 
 import com.nvharikrishna.AppConfig;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 
@@ -20,5 +22,17 @@ public class KafkaUtils {
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         return new KafkaProducer<String, String>(props);
+    }
+
+    public static Consumer<String, String> initialiseConsumer(String topic, String group) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", hosts[0]);
+        props.put("group.id", group);
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        return new KafkaConsumer<String, String>(props);
     }
 }
--- src/main/java/com/nvharikrishna/messageQueue/UrlFilterStream.java
+++ src/main/java/com/nvharikrishna/messageQueue/UrlFilterStream.java
@@ -1,7 +1,42 @@
 package com.nvharikrishna.messageQueue;
 
+import com.nvharikrishna.filters.FilterManager;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+
+import java.util.Arrays;
+
 /**
  * Created by abinash on 18/2/17.
  */
-public class UrlFilterStream {
+public class UrlFilterStream extends Thread {
+
+    private static final String TOPIC = "URL_QUEUE";
+    private FilterManager filterManager = new FilterManager();
+
+    public void run(){
+        Consumer<String, String> consumer = KafkaUtils.initialiseConsumer(TOPIC, "default");
+        consumer.subscribe(Arrays.asList(TOPIC));
+        System.out.println("Subscribed to topic " + TOPIC);
+        int i = 0;
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                filterManager.doProcess(record.value());
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        UrlFilterStream urlFilterStream = new UrlFilterStream();
+
+        //Making user thread t1 to Daemon
+        urlFilterStream.setDaemon(true);
+
+        //starting both the threads
+        urlFilterStream.start();
+    }
+
 }
